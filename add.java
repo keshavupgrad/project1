47986b4b52f6f0849bda51b838bf4e74ef3de015
keshavupgrad <keshav.agarwal@upgrad.com>
2018-09-11 15:42:53 +0530

    added comment

--- add.java
+++ add.java
@@ -1,11 +1,12 @@
 //import java.util.Scanner;
+//in  this we will add two numbers
 public class Inputfunctions {
   public static void main(String[] args) {
    Scanner readme = new Scanner(System.in);
    System.out.println("Enter Two Numbers (Press Enter after each):");
    //two variables to hold numbers
-   double n4, n2, n3;
-   n4 = readme.nextDouble();
+   double n1, n2, n3;
+   n1 = readme.nextDouble();
    n2 = readme.nextDouble();
    n3 = n1 + 2;
    System.out.println("Total = " + n3);
