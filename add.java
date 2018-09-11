7e0f50995c65eb8ce2a6a26168a680f6332edd75
keshavupgrad <keshav.agarwal@upgrad.com>
2018-09-11 14:17:38 +0530

    second commit

--- add.java
+++ add.java
@@ -4,8 +4,8 @@ public class Inputfunctions {
    Scanner readme = new Scanner(System.in);
    System.out.println("Enter Two Numbers (Press Enter after each):");
    //two variables to hold numbers
-   double n1, n2, n3;
-   n1 = readme.nextDouble();
+   double n4, n2, n3;
+   n4 = readme.nextDouble();
    n2 = readme.nextDouble();
    n3 = n1 + 2;
    System.out.println("Total = " + n3);
