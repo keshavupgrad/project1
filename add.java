d9f805b86316cc3016838209123d8289d3802138
keshavupgrad <keshav.agarwal@upgrad.com>
2018-09-12 12:02:16 +0530

    second day second comment

--- add.java
+++ add.java
@@ -4,11 +4,13 @@ public class Inputfunctions {
    Scanner readme = new Scanner(System.in);
    System.out.println("Enter Two Numbers (Press Enter after each):");
    //two variables to hold numbers
-   double n4, n2, n3;
-   n4 = readme.nextDouble();
+   double n1, n2, n3;
+   n1 = readme.nextDouble();
    n2 = readme.nextDouble();
    n3 = n1 + 2;
    System.out.println("Total = " + n3);
   }
 }
 //is this fil changed
+//now this was the first change i mae on second day
+//sesond day second change
